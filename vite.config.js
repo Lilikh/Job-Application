775cfc8958f50bc7b7b869b7435bc245d2ceff1a
Leila Kheirandish <81739933+Lilikh@users.noreply.github.com>
2024-11-26 14:25:58 +0100

    add sth in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'primeicons': '/node_modules/primeicons',
     },
   },
 })
